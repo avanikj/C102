8896af7fd9d35fb3845f01b4cf81a6715fd829a7
avanikj <71885113+avanikj@users.noreply.github.com>
2022-11-04 20:02:46 +0530

    Add files via upload

---
+++ organized.py
@@ -0,0 +1,6 @@
+import os
+import shutil
+source = "pictures/p1.jpeg"
+#destination = "/home/ambily/Desktop/Projects/Python/pictures/copyp1.jpeg"
+#abc = shutil.copy(source,destination)
+print(os.listdir(source))
